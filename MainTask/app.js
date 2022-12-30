ea24faf7c8cac60f1251dd76a7fc3038eceeafc4
Sajjad <heybrosajjad@gmail.com>
2022-12-30 10:03:28 +0500

    got all ads data as a object

--- MainTask/app.js
+++ MainTask/app.js
@@ -6,6 +6,61 @@ const pretty = require("pretty");
 // URL of the page we want to scrape
 const url = "https://www.otomoto.pl";
 
+// Geting Desire Data Of Every Ad From This Function
+function getSingleAdAllData($, el) {
+  // helper Variables
+  let adId, adTitle, adPrice, adImage;
+  let production_date, mileage, fuel_type, power;
+
+  adId = $(el).data("testid");
+
+  adTitle = $(el)
+    .children("article")
+    .children("section")
+    .children("div")
+    .children("h2")
+    .text();
+
+  adPrice = $(el)
+    .children("article")
+    .children("section")
+    .children("section")
+    .children("div")
+    .text();
+
+  adImage = $(el).children("article").children("img").eq(0).attr("src");
+
+  allListItems = $(el)
+    .children("article")
+    .children("section")
+    .children("div")
+    .children("section")
+    .children("ul");
+
+  // geting ad other data
+  allListItems.each((idx, el) => {
+    const listData = [];
+
+    const singleListItemsList = $(el).contents();
+    singleListItemsList.each((idx, el) => {
+      const text = $(el).children("span").text();
+      listData.push(text);
+    });
+
+    [production_date, mileage, fuel_type, power] = listData;
+  });
+  return {
+    adId,
+    adImage,
+    adTitle,
+    adPrice,
+    production_date,
+    mileage,
+    fuel_type,
+    power,
+  };
+}
+
 // Async function which scrapes the data
 async function scrapeData() {
   try {
@@ -15,58 +70,16 @@ async function scrapeData() {
     const adsHtmlList = $(".eomulv92");
 
     adsHtmlList.each(function (idx, el) {
-      console.log(idx);
-
-      const adId = $(el).data("testid");
-
-      const adTitle = $(el)
-        .children("article")
-        .children("section")
-        .children("div")
-        .children("h2")
-        .text();
-
-      const adPrices = $(el)
-        .children("article")
-        .children("section")
-        .children("section")
-        .children("div")
-        .text();
-
-      const adImage = $(el)
-        .children("article")
-        .children("img")
-        .eq(0)
-        .attr("src");
-
-      const adOthersDataHtml = $(el)
-        .children("article")
-        .children("section")
-        .children("div")
-        .children("section");
-
-      const listItems = $(el)
-        .children("article")
-        .children("section")
-        .children("div")
-        .children("section")
-        .children("ul")
-
-        listItems.each((idx, el) => {
-          
-        })
-        
-
-      // console.log(listItems);
-
-      // console.log(listItemsArray);
-      // get other list item value
-      // listItemsArray.forEach((el, i) => {
-      //   console.log(el);
-      //   const name = el.children("span").text();
-      //   console.log(name);
-      // });
-      // console.log(adId);
+      const {
+        adId,
+        adImage,
+        adTitle,
+        adPrice,
+        production_date,
+        mileage,
+        fuel_type,
+        power,
+      } = getSingleAdAllData($, el);
     });
   } catch (err) {
     console.log(err);
