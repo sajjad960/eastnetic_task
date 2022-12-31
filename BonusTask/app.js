97aaec52c90b6d9f32cba071b060cedbae3bb6a6
Sajjad <heybrosajjad@gmail.com>
2022-12-31 12:09:04 +0500

    completed main task

---
+++ BonusTask/app.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+
+const url =
+  "https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc";
+
+let
